87c7c1c14f903595bcbf0d9a41ddf6a975bd3eb3
Binh Thanh Nguyen <59437632+thanhbinh030296@users.noreply.github.com>
2024-02-05 14:59:35 +0700

    update hihi

---
+++ dsa-using-C-2nd/chapter1/programming-exervises/ex10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// programing exercises 10. page 40
+int main()
+{
+    printf("\nBBB     Y       Y     EEEE");
+    printf("\nB   B     Y    Y      E");
+    printf("\nBBB          Y        EEEE");
+    printf("\nB   B        Y");
+    return 0;
+}
