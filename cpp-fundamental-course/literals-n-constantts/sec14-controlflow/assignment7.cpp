ef573519552f9abcc622bc977dc19d39a246d870
Binh Thanh Nguyen <59437632+thanhbinh030296@users.noreply.github.com>
2024-02-25 20:48:06 +0700

    update dsa

---
+++ cpp-fundamental-course/literals-n-constantts/sec14-controlflow/assignment7.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int maint()
+{
+    cout << "Enter a day " << endl;
+    int day;
+    string today;
+    cin >> day;
+    bool isContinue = true;
+    switch (day)
+    {
+    case 1:
+        today = "Monday";
+        break;
+    case 2:
+        today = "Tuesday";
+        break;
+    case 3:
+        today = "Wednesday";
+        break;
+    case 4:
+        today = "Thursday";
+        break;
+    case 5:
+        today = "Friday";
+        break;
+    case 6:
+        today = "Saturday";
+        break;
+    case 7:
+        today = "Sunday";
+        break;
+    default:
+        isContinue = false;
+        break;
+    }
+
+    if (isContinue == false)
+        cout << day << " is not a valid day. Bye!" << endl;
+    else
+    {
+        cout << "How many days have passed up to day" << endl;
+        int n_day;
+        cin >> n_day;
+
+        cout << "Today is " << today << endl;
+
+        if (n_day % 7 == 0)
+        {
+            cout << "If we went " << n_day << " days in the past we would hit a " << today <<endl; 
+        }
+        else {
+            int m_multiple = n_day / 7 + 1;
+            int passed_day = day + m_multiple - n_day;
+            if (passed_day > 7)
+                passed_day -=7;
+        }
+    }
+
+    return 0;
+}
---
+++ cpp-fundamental-course/literals-n-constantts/sec14-controlflow/flow.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+constexpr bool condition {true};
+
+int main()
+{
+    // tennary expresison
+
+    int k = 1;
+    int hihi = (k > 0) ? 12 : -100;
+
+    cout << "\nTernary expression: " << hihi << endl;
+
+    // const express
+    condition = false;
+    if constexpr(condition)
+    {
+        cout << "const express TRUE" << endl;
+    }
+    else
+        cout << "const expr FALSE" << endl;
+
+    return 0;
+}
