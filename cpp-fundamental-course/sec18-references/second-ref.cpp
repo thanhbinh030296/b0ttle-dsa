f1bdf3602063e68bc085d6a57bb2c5e27bb14634
b0ttle-mac <thanhbinh030296@gmail.com>
2024-03-03 22:08:59 +0700

    update from MacOS

--- cpp-fundamental-course/sec18-references/second-ref.cpp
+++ cpp-fundamental-course/sec18-references/second-ref.cpp
@@ -6,7 +6,7 @@ int main()
     int age{22};
 
     int *age_ptr;
-    int &age_ref{age};
+    int& age_ref {age};
     age_ptr = &age;
     std::cout << "age addr1 : " << age_ptr << std::endl;
     std::cout << "age addr2 : " << &age_ref << std::endl;
